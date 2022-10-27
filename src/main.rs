6a5f6baa19c07e10b8674bac62a55376839dc97c
Michael Brogdon <michael.brogdon@monotype.com>
2022-10-27 10:42:13 -0600

    Output glyphs into directory specific to font

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-extern crate auto_args;
-
 use image::{DynamicImage, Rgba};
-
 use log::debug;
-
-use std::fs;
-
 use owned_ttf_parser::{AsFaceRef, OwnedFace};
-
-use crate::auto_args::AutoArgs;
-
+use rustc_serialize::{self, hex::ToHex};
 use rusttype::{point, Font, Scale};
+use std::fs;
+use auto_args::AutoArgs;
 
 /// Structure for the command line arguments.
 #[derive(Debug, AutoArgs)]
@@ -26,19 +20,25 @@ impl Default for CliArgs {
     fn default() -> Self {
         Self {
             font_file: Default::default(),
-            output_dir: Some("out".to_owned())
+            output_dir: Some("out".to_owned()),
         }
     }
+}
+
+fn get_cli_args() -> Result<CliArgs, Box<dyn std::error::Error>> {
+    let mut args = CliArgs::from_args();
+    if args.output_dir.is_none() {
+        debug!("Output directory was not specified, using default.");
+        let default_args: CliArgs = CliArgs::default();
+        args.output_dir = default_args.output_dir;
+    }
+    Ok(args)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    let mut arguments = CliArgs::from_args();
-    debug!("Command line arguments: {:#?}", &arguments);
-    if arguments.output_dir.is_none() {
-        arguments.output_dir = Some("out".to_owned());
-    }
+    let arguments = get_cli_args()?;
     debug!("Command line arguments: {:#?}", &arguments);
 
     let font_data = std::fs::read(&arguments.font_file)?;
@@ -46,7 +46,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: Use the font_face to get number of glyphs to draw
     font_face.as_face_ref().tables().maxp.number_of_glyphs.get();
     //font_face.as_face_ref()
-    let valid_unicode_range = ('\u{0000}'..'\u{10FFFF}').filter(|c| c.is_alphabetic() || c.is_alphanumeric());
+    let valid_unicode_range =
+        ('\u{0000}'..'\u{10FFFF}').filter(|c| c.is_alphabetic() || c.is_alphanumeric());
     let font_data = std::fs::read(&arguments.font_file)?;
     let font = Font::try_from_vec(font_data).unwrap_or_else(|| {
         panic!(
@@ -63,9 +64,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //let mut count = 0;
     for unicode in valid_unicode_range {
         let glyph = font.glyph(unicode);
-        let positioned_glyph = glyph
-            .scaled(scale)
-            .positioned(point(0.0, v_metrics.ascent));
+        let positioned_glyph = glyph.scaled(scale).positioned(point(0.0, v_metrics.ascent));
         debug!("Dealing with unicode: {:?}", &unicode);
 
         if let Some(bounding_box) = positioned_glyph.pixel_bounding_box() {
@@ -78,7 +77,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut image = DynamicImage::new_rgba8(glyph_width + 45, glyph_height + 45).to_rgba8();
             positioned_glyph.draw(|x, y, v| {
                 image.put_pixel(
-                    x + bounding_box.min.x as u32,
+                    x + bounding_box.min.x.unsigned_abs() as u32,
                     y + bounding_box.min.y as u32,
                     Rgba([
                         output_color.0,
@@ -89,17 +88,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 );
             });
             if let Some(output_dir) = arguments.output_dir.as_ref() {
+                let base_name = std::path::Path::new(&arguments.font_file)
+                    .file_name()
+                    .unwrap_or_else(|| {
+                        panic!("Error getting file name");
+                    })
+                    .to_str()
+                    .unwrap_or_else(||{
+                        panic!("Error converting to string.");
+                    });
+                let output_dir = &format!("{}/{}", output_dir, base_name);
                 fs::create_dir_all(output_dir).or_else(|e| {
-                    debug!("Could not create directory, most likely already exists: {:?}", e);
+                    debug!(
+                        "Could not create directory, most likely already exists: {:?}",
+                        e
+                    );
                     Ok::<(), Box<dyn std::error::Error>>(())
                 })?;
-                image.save(format!(
-                    "{}/{}_image.png",
-                    output_dir,
-                    unicode
-                ))?;
-            }
-            else {
+                let hex_name = &unicode.to_string().as_bytes().to_hex();
+                image.save(format!("{}/{}_image.png", &output_dir, &hex_name))?;
+            } else {
                 panic!("Output directory is not specified!");
             }
         }
