82a544db2ff244887ba1639e18c19ea69a0243cf
Michael Brogdon <michael.brogdon@monotype.com>
2022-10-27 11:56:45 -0600

    Working example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use auto_args::AutoArgs;
 use image::{DynamicImage, Rgba};
 use log::debug;
 use owned_ttf_parser::{AsFaceRef, OwnedFace};
 use rustc_serialize::{self, hex::ToHex};
 use rusttype::{point, Font, Scale};
 use std::fs;
-use auto_args::AutoArgs;
 
 /// Structure for the command line arguments.
 #[derive(Debug, AutoArgs)]
@@ -46,8 +46,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: Use the font_face to get number of glyphs to draw
     font_face.as_face_ref().tables().maxp.number_of_glyphs.get();
     //font_face.as_face_ref()
-    let valid_unicode_range =
-        ('\u{0000}'..'\u{10FFFF}').filter(|c| c.is_alphabetic() || c.is_alphanumeric());
     let font_data = std::fs::read(&arguments.font_file)?;
     let font = Font::try_from_vec(font_data).unwrap_or_else(|| {
         panic!(
@@ -55,30 +53,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             &arguments.font_file
         );
     });
-    let scale = Scale::uniform(512.0);
+    let valid_unicode_ranges =
+        ('\u{0000}'..'\u{10FFFF}').filter(|c| c.is_alphabetic() || c.is_alphanumeric());
     // Use a black color as output
     let output_color = (255, 255, 255);
-
+    // Scale by a uniform factor
+    let scale = Scale::uniform(512.0);
+    // Grab the vertical metrics for the font at the specified scale
     let v_metrics = font.v_metrics(scale);
 
-    //let mut count = 0;
-    for unicode in valid_unicode_range {
+    for unicode in valid_unicode_ranges {
         let glyph = font.glyph(unicode);
-        let positioned_glyph = glyph.scaled(scale).positioned(point(0.0, v_metrics.ascent));
-        debug!("Dealing with unicode: {:?}", &unicode);
+        let id = glyph.id().0;
+        if id == 0x20DE || id == 0 {
+            continue;
+        }
+        let positioned_glyph = glyph
+            .scaled(scale)
+            .positioned(point(0.0, 0.0));
+        debug!("Dealing with unicode: {:?}", unicode);
+        debug!("\tv_metrics: {:#?}", &v_metrics);
 
         if let Some(bounding_box) = positioned_glyph.pixel_bounding_box() {
-            let glyph_height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
+            debug!("\tBounding box: {:#?}", &bounding_box);
+            let glyph_height = {
+                let min_y = bounding_box.min.y;
+                let max_y = bounding_box.max.y;
+                (min_y - max_y).unsigned_abs() as u32
+            };
             let glyph_width = {
                 let min_x = bounding_box.min.x;
                 let max_x = bounding_box.max.x;
                 (max_x - min_x) as u32
             };
-            let mut image = DynamicImage::new_rgba8(glyph_width + 45, glyph_height + 45).to_rgba8();
+            debug!("Glyph WxH: {}x{}", &glyph_width, &glyph_height);
+            let mut image = DynamicImage::new_rgba8(glyph_width, glyph_height).to_rgba8();
             positioned_glyph.draw(|x, y, v| {
                 image.put_pixel(
-                    x + bounding_box.min.x.unsigned_abs() as u32,
-                    y + bounding_box.min.y as u32,
+                    x as u32,
+                    y as u32,
                     Rgba([
                         output_color.0,
                         output_color.1,
@@ -94,7 +107,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         panic!("Error getting file name");
                     })
                     .to_str()
-                    .unwrap_or_else(||{
+                    .unwrap_or_else(|| {
                         panic!("Error converting to string.");
                     });
                 let output_dir = &format!("{}/{}", output_dir, base_name);
@@ -105,18 +118,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     );
                     Ok::<(), Box<dyn std::error::Error>>(())
                 })?;
-                let hex_name = &unicode.to_string().as_bytes().to_hex();
+                let hex_name = unicode.to_string().as_bytes().to_hex();
                 image.save(format!("{}/{}_image.png", &output_dir, &hex_name))?;
             } else {
                 panic!("Output directory is not specified!");
             }
         }
-        /*
-        count = count + 1;
-        if count >= 10 {
-            break;
-        }
-        */
     }
     Ok(())
 }
